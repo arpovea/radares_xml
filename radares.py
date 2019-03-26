30d4a659c03b670816cbff9db1c1822bc6ddb416
Adrian R <arodriguezpovea@gmail.com>
2019-03-26 12:35:35 +0100

    Añadiendo menu a radares.py

---
+++ radares.py
@@ -0,0 +1,14 @@
+from lxml import etree
+doc= etree.parse('radares.xml')
+
+while True:
+	print ('''
+	Elige una opcion:
+	1-Muestra el nombre de las provincias de las que tenemos información sobre radares.
+	2-Muestra la cantidad de radares de los que tenemos información.
+	3-Pide por teclado una provincia, muestra el nombre de la carretera que tiene y la cantidad de radares.
+	4-Pide por teclado una carretera, muestra el nombre de las provincias por las que pasa y sus radares.
+	5-Pide por teclado una carretera, cuenta los radares que tiene y muestra las cordernadas de los radares.
+	0-Para salir.
+	''')
+	opcion=int(input("Opcion: "))
